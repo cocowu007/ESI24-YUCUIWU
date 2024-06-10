4f43d913f7777c8e65c3dfe1c35b3eb501013037
Yucui Wu <yucui_wu@163.com>
2024-06-10 16:14:10 +0300

    task 3

--- drservice/src/main/java/com/esi/drservice/service/DrService.java
+++ drservice/src/main/java/com/esi/drservice/service/DrService.java
@@ -1,11 +1,39 @@
 package com.esi.drservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.esi.drservice.repository.DrRepository;
+import com.esi.drservice.model.Appointment;
+import com.esi.drservice.dto.AppointmentDto;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.kafka.annotation.KafkaListener;
 
 @RequiredArgsConstructor
 @Service
 public class DrService {
+
+    @Autowired
+    private DrRepository drRepository;
+
+    @KafkaListener(topics = "appointmentTopic", groupId = "appointEventGroup" )
+    public void processAppointment(AppointmentDto appointmentDto) {
+
+        Appointment appointment = mapToAppointment(appointmentDto);
+        drRepository.save(appointment);        
+
+    }
+
+    private Appointment mapToAppointment(AppointmentDto appointmentDto) {
+
+        return Appointment.builder()
+                .appointmentid(appointmentDto.getAppointmentid())
+                .patientId(appointmentDto.getPatientId())
+                .patientName(appointmentDto.getPatientName())
+                .drId(appointmentDto.getDrId())
+                .date(appointmentDto.getDate())
+                .build();
+    }
 
 }
