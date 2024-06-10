d61621e8922d0b727e9209d1bd2aaa29049e3b4f
Yucui Wu <yucui_wu@163.com>
2024-06-10 15:35:04 +0300

    task 1

--- patientservice/src/main/java/com/esi/patientservice/service/PatientService.java
+++ patientservice/src/main/java/com/esi/patientservice/service/PatientService.java
@@ -1,8 +1,11 @@
 package com.esi.patientservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.esi.patientservice.dto.PatientDto;
+import com.esi.patientservice.model.Patient;
+import com.esi.patientservice.repository.PatientRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,8 +13,23 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PatientService {
 
+    @Autowired
+    private PatientRepository patientRepository;
+
     public void addPatientInfo(PatientDto patientDto) {
 
+        Patient patient = mapToPatient(patientDto);
+        patientRepository.save(patient);
+
+    }
+
+    private Patient mapToPatient(PatientDto patientDto) {
+
+        return Patient.builder()
+                .patientId(patientDto.getPatientId())
+                .patientName(patientDto.getPatientName())
+                .patientData(patientDto.getPatientData())
+                .build();
     }
 
 }
