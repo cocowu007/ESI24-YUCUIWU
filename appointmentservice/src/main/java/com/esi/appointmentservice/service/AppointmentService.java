f577d85fc3f131b7d8ce7f21bd65f4c3e359ba7f
Yucui Wu <yucui_wu@163.com>
2024-06-10 15:42:34 +0300

    task 2.1

--- appointmentservice/src/main/java/com/esi/appointmentservice/service/AppointmentService.java
+++ appointmentservice/src/main/java/com/esi/appointmentservice/service/AppointmentService.java
@@ -1,8 +1,11 @@
 package com.esi.appointmentservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.esi.appointmentservice.dto.AppointmentDto;
+import com.esi.appointmentservice.model.Appointment;
+import com.esi.appointmentservice.repository.AppointmentRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,8 +13,24 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class AppointmentService {
 
+  @Autowired
+  private AppointmentRepository appointmentRepository;
+
   public void appointmentBooked(AppointmentDto appointmentDto) {
 
+    Appointment appointment = mapToAppointment(appointmentDto);
+    appointmentRepository.save(appointment);
   }
+
+  private Appointment mapToAppointment(AppointmentDto appointmentDto) {
+
+    return Appointment.builder()
+            .appointmentid(appointmentDto.getAppointmentid())
+            .patientId(appointmentDto.getPatientId())
+            .patientName(appointmentDto.getPatientName())
+            .drId(appointmentDto.getDrId())
+            .date(appointmentDto.getDate())
+            .build();
+}
 
 }
